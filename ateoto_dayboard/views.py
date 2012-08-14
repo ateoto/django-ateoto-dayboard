7bf2534989cbe75b1a2eb00f4d76f87903d1a29c
Matthew McCants <mattmccants@gmail.com>
2012-08-14 03:56:32 -0500

    urlencode scope. TODO: Use requests library for this?

--- ateoto_dayboard/views.py
+++ ateoto_dayboard/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 
 import requests
 from requests_oauth2 import OAuth2
+from urllib import urlencode
 
 def auth(request):
 
@@ -15,7 +16,7 @@ def auth(request):
     authorize = "/o/oauth2/auth"
     token = "/o/oauth2/token"
 
-    scope = "https://www.googleapis.com/auth/calendar+https://www.googleapis.com/auth/plus.me+https://www.googleapis.com/auth/tasks+https://www.googleapis.com/auth/userinfo.profile"
+    scope = urlencode("https://www.googleapis.com/auth/calendar+https://www.googleapis.com/auth/plus.me+https://www.googleapis.com/auth/tasks+https://www.googleapis.com/auth/userinfo.profile")
 
     redirect_uri =  request.build_absolute_uri(reverse('dayboard-callback'))
 
