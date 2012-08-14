466c356c55df668baf76790f787e19e467b98d7b
Matthew McCants <mattmccants@gmail.com>
2012-08-14 04:01:15 -0500

    urlencode scope.

--- ateoto_dayboard/views.py
+++ ateoto_dayboard/views.py
@@ -16,7 +16,12 @@ def auth(request):
     authorize = "/o/oauth2/auth"
     token = "/o/oauth2/token"
 
-    scope = urlencode("https://www.googleapis.com/auth/calendar+https://www.googleapis.com/auth/plus.me+https://www.googleapis.com/auth/tasks+https://www.googleapis.com/auth/userinfo.profile")
+    scope = urlencode([
+        "https://www.googleapis.com/auth/calendar",
+        "https://www.googleapis.com/auth/plus.me",
+        "https://www.googleapis.com/auth/tasks",
+        "https://www.googleapis.com/auth/userinfo.profile"
+    ])
 
     redirect_uri =  request.build_absolute_uri(reverse('dayboard-callback'))
 
