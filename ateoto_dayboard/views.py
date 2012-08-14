777ec9d31006d73cc91015ce7b7a45177cee1efa
Matthew McCants <mattmccants@gmail.com>
2012-08-14 15:05:57 -0500

    Using requestb.in for debug.

--- ateoto_dayboard/views.py
+++ ateoto_dayboard/views.py
@@ -43,10 +43,10 @@ def callback(request):
     if request.method == 'GET':
         code = request.GET.get('code', None)
         if code:
-            url = '%s%s' % (site, token)
+            url = 'http://requestb.in/n01vl0n0'
             payload = { 'code' : code,
                         'client_id' : client_id,
-                        'client_secret' : client_secret,
+                        'client_secret' : 'fake',
                         'redirect_uri' : redirect_uri,
                         'grant_type' : 'authorization_code'}
             #response = oauth2_handler.get_token(code)
