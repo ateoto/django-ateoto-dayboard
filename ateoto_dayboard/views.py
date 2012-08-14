0c4ff50ce48e64cd9f789d0d13ca1aa8cdd478dc
Matthew McCants <mattmccants@gmail.com>
2012-08-14 13:45:37 -0500

    Started callback function.

--- ateoto_dayboard/views.py
+++ ateoto_dayboard/views.py
@@ -25,4 +25,27 @@ def auth(request):
     return HttpResponse('<a href="%s">Click</a>' % auth_url)
 
 def callback(request):
-    return HttpResponse('cool')
+    # This is a lot of duplicated work, put this in a function somewhere?
+    client_id = settings.GOOGLE_OAUTH2_CLIENT_ID
+    client_secret = settings.GOOGLE_OAUTH2_CLIENT_SECRET
+
+    site = "http://accounts.google.com"
+
+    authorize = "/o/oauth2/auth"
+    token = "/o/oauth2/token"
+
+    scope = "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile"
+
+    redirect_uri =  request.build_absolute_uri(reverse('dayboard-callback'))
+
+    oauth2_handler = OAuth2(client_id, client_secret, site, redirect_uri, authorize, token)
+
+    if request.method == 'GET':
+        code = request.GET.get('code', None)
+        if code:
+            #response = oauth2_handler.get_token(code)
+            return HttpResponse(code)
+        else:
+            return HttpResponse('Code not found')
+    else:
+        return HttpResponse('Not GET')
